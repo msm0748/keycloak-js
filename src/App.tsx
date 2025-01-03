4be6d5bf9995d93ad1b1ed5c4b2558f252cdf5f7
seokmin <msm0748@gmail.com>
2025-01-03 16:52:06 +0900

    feat: 유저 role 조회 구현

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,18 @@ function App() {
     }
 
     keycloak.loadUserInfo().then((userInfo) => {
-      console.log(userInfo);
+      console.log(userInfo); // 유저 정보 조회
     });
+
+    const roles =
+      keycloak.tokenParsed?.resource_access?.['testApp']?.roles || [];
+
+    roles.forEach((role) => {
+      console.log(role); // admin, user 등의 역할 출력
+    });
+
+    // accessToken 출력
+    const accessToken = keycloak.token;
   }, []);
   return <div>메인</div>;
 }
